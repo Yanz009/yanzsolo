02ee64b52520b5976656724ef10e944b18bb6853
Yanz009 <adojoah@gmail.com>
2025-11-08 13:14:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "7716530870:AAGVV1Z5YCiJNzHD5CicaWx_9hpFGQI6KVE",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7023760854", // Ubah Jadi Id Mu !!!
+  port: "2009", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "http://resseler-aryastore.aryastore.biz.id" // Ubah Jadi Ip Vps Mu !!!
+};
